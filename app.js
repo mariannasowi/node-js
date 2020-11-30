98f19afa77fd092a97d2de9377f4386058e10f6d
Marianna Sowinska <marianna.sowi@gmail.com>
2020-11-30 22:59:07 +0100

    Module 26.2. Task: identity generator

---
+++ app.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const genders=['M', 'F'];
+
+const maleNames=['Mike', 'Ken'];
+
+const femaleNames=['Anne', 'Kate'];
+
+const lastNames=['Jordan', 'Robertson'];
+
+function randChoice(arr) {
+    const random = Math.floor(Math.random() * arr.length);
+    return arr[random];
+};
+
+const people = [];
+
+for(i=0; i<20; i++){
+    
+    var person = {};
+
+    person.gender = randChoice(genders);
+
+    if(person.gender == 'M') {
+       person.firstName = randChoice(maleNames); 
+    } else {
+        person.firstName = randChoice(femaleNames);
+    }
+    
+    person.lastName = randChoice(lastNames)
+    person.age = Math.floor(Math.random() * (18 - 1)) + 1;
+
+    people.push(person);
+}
+
+var peopleJson = JSON.stringify(people);
+
+console.log(people)
+
+fs.writeFile('people.json', peopleJson, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
